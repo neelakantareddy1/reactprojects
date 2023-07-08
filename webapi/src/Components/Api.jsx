8256446cf24fe8e06543b824d55ae02325bd3469
imprasanth2000 <battiniprasanth2000@gmail.com>
2023-07-08 21:44:26 +0530

    aboutconnecting api

--- webapi/src/Components/Api.jsx
+++ webapi/src/Components/Api.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
-import { getList } from "../Services/list";
+import { getList, additem } from "../Services/list";
 function Api() {
   const [list, setList] = useState([]);
+  const [itemInput, setItemInput] = useState('');
+  const [alert, setAlert] = useState(false);
   const info = () => {
     let mounted = true;
     getList().then((items) => {
@@ -12,11 +14,30 @@ function Api() {
     return () => (mounted = false);
   };
   useEffect(info, []);
+  const handleChange = (event) => {
+       setItemInput(event.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    additem(itemInput)
+      .then(() => {
+        setItemInput('');
+        setAlert(true)
+      })
+  };
   return <>
   <h1>My Grocery List</h1>
      <ol>
        {list.map(item => <li key={item.item}>{item.item}</li>)}
      </ol>
+     {alert && <h2>submit successful</h2>}
+     <form onSubmit={handleSubmit}>
+        <label>
+            <h4>New Item</h4>
+            <input type="text" onChange={handleChange} value={itemInput}/>
+        </label>
+        <button type="submit">Submit</button>
+     </form>
   </>;
 }
 
--- webapi/src/Services/list.js
+++ webapi/src/Services/list.js
@@ -4,3 +4,14 @@ export function getList() {
     const URL = "http://localhost:3333/list";
     return fetch(URL).then(data => data.json());
 }
+
+export function additem (item) {
+    const URL = "http://localhost:3333/list";
+    return fetch(URL, {
+        method : 'POST',
+        headers : {
+            'content-type' : 'application/json'
+        },
+        body : JSON.stringify({item})
+    }).then(data => data.json());
+}
