0213264d814bae637b65eb813989d052db694119
imprasanth2000 <battiniprasanth2000@gmail.com>
2023-07-09 10:15:01 +0530

    updating groceries list and handling alert message

--- webapi/src/Components/Api.jsx
+++ webapi/src/Components/Api.jsx
@@ -4,8 +4,23 @@ function Api() {
   const [list, setList] = useState([]);
   const [itemInput, setItemInput] = useState('');
   const [alert, setAlert] = useState(false);
+
+  // To hide alert message after few seconds 
+
+    useEffect(() => {
+      if (alert) {
+        setTimeout(() => {
+          setAlert(false);
+        }, 5000);
+      }
+    }, [alert]);
+
+
   const info = () => {
     let mounted = true;
+    if(list.length && !alert){
+      return;
+    }
     getList().then((items) => {
       if (mounted) {
         setList(items);
@@ -13,10 +28,12 @@ function Api() {
     });
     return () => (mounted = false);
   };
-  useEffect(info, []);
+  useEffect(info, [list, alert]);
+
   const handleChange = (event) => {
-       setItemInput(event.target.value)
+       setItemInput(event.target.value);
   }
+
   const handleSubmit = (e) => {
     e.preventDefault();
     additem(itemInput)
@@ -31,6 +48,7 @@ function Api() {
        {list.map(item => <li key={item.item}>{item.item}</li>)}
      </ol>
      {alert && <h2>submit successful</h2>}
+
      <form onSubmit={handleSubmit}>
         <label>
             <h4>New Item</h4>
